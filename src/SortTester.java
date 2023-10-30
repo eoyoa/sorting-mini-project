558cf904079c2492ca070c379ce176d3f1ab0e2a
kerabite <10791585+kerabite@users.noreply.github.com>
2023-10-30 10:51:27 -0500

    Added five test cases for Sorter<T> objects

--- src/SortTester.java
+++ src/SortTester.java
@@ -1,10 +1,6 @@
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
-import java.math.BigInteger;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -44,5 +40,48 @@ public class SortTester {
     sorter.sort(original, (x, y) -> x.compareTo(y));
     assertArrayEquals(original, expected);
   } // orderedStringTest
+
+  // New test cases
+  @Test
+  public void orderedIntegerTest() {
+     Integer[] original = { 1, 2, 3, 4, 5 };
+     Integer[] expected = original.clone();
+     sorter.sort(original, (x, y) -> x.compareTo(y));
+     assertArrayEquals(original, expected);
+  }
+
+  @Test
+  public void reverseOrderedIntegerTest() {
+     Integer[] original = { 5, 4, 3, 2, 1 };
+     Integer[] expected = { 1, 2, 3, 4, 5 };
+     sorter.sort(original, (x, y) -> x.compareTo(y));
+     assertArrayEquals(original, expected);
+  }
+
+  @Test
+  public void integersWithDupesTest() {
+     Integer[] original = { 5, 1, 3, 1, 3, 2, 6 };
+     Integer[] expected = { 1, 1, 2, 3, 3, 5, 6 };
+     sorter.sort(original, (x, y) -> x.compareTo(y));
+     assertArrayEquals(original, expected);
+  }
+
+  @Test
+  public void stringLengthTest() {
+    String[] original = { "alpha", "bravo", "charlie", "delta", "foxtrot" };
+    int[] expectedLengths = { 5, 5, 5, 7, 7 };
+    sorter.sort(original, (x, y) -> x.length() - y.length());
+    for (int i = 0; i < original.length; i++) {
+      assertEquals(original[i].length(), expectedLengths[i]);
+    }
+  }
+
+  @Test
+  public void floatTest() {
+    Float[] original = { -0.01f, 2f, 1f, -2f, 0f, 1.01f, 0.1f, 0.1f };
+    Float[] expected = { -2f, -0.01f, 0f, 0.1f, 0.1f, 1f, 1.01f, 2f };
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(original, expected);
+  }
 
 } // class SortTester
