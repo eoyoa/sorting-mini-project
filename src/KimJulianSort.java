3cef81f0d6a2ff7228e9ded47bcc5b59f0ad8569
kerabite <10791585+kerabite@users.noreply.github.com>
2023-10-31 08:23:48 -0500

    Start own sort and clean up files

---
+++ src/KimJulianSort.java
@@ -0,0 +1,11 @@
+import java.util.Comparator;
+
+public class KimJulianSort implements Sorter {
+
+  @Override
+  public <T> void sort(T[] values, Comparator<? super T> order) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'sort'");
+  }
+  
+}
